42fdef2b3171d6c401a3f1b026074b0491e6818c
kakimaru <memimei0306@gmail.com>
2024-09-21 08:49:35 +0900

    I tried exercise

--- tests/convertTemp.test.js
+++ tests/convertTemp.test.js
@@ -3,6 +3,14 @@
 // Test case 2: Converting 104 deg fahrenheit will return 40 deg celsius
 // Both test cases need to pass
 
-const Converter = require('../src/convertTemp')
+const Converter = require("../src/convertTemp");
 
-const convert = new Converter()
+const convert = new Converter();
+
+test("Converting 30 deg celsius will return 86 deg fahrenheit", () => {
+  expect(convert.convertToFahrenheit(30)).toBe(86);
+});
+
+test('Converting 104 deg fahrenheit will return 40 deg celsius', () => {
+  expect(convert.convertToCelsius(104)).toBe(40)
+})
--- tests/counter.test.js
+++ tests/counter.test.js
@@ -1,7 +1,15 @@
 // Create two test cases for incrementing and decrementing a number
-// Test case 1: counter(10, 'increment') will eventualy return 10
+// Test case 1: counter(10, 'increment') will eventually return 10
 // Test case 2: counter(10, 'decrement') will eventually return 0
 // Both test cases need to pass
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
-const counter = require('../src/counter')
+const counter = require("../src/counter");
+
+test("counter(3, 'increment') will eventually return 3", () => {
+  return expect(counter(3, "increment")).resolves.toBe(3);
+});
+
+test("counter(3, 'decrement') will eventually return 0", () => {
+  return expect(counter(3, "decrement")).resolves.toBe(0);
+});
